259f55d89aae7ab2dc22a88a306f64e04132e435
Huby Franck <franck.huby@loamok.org>
2016-03-15 11:41:51 +0100

    Cleaning the code, Adding 'events functionality' Adding 'Calendar' chart definition

--- Twig/GChartExtension.php
+++ Twig/GChartExtension.php
@@ -27,80 +27,100 @@ class GChartExtension extends \Twig_Extension {
      */
     public function getFunctions() {
         return array(
-            'gchart_get_qrcode_url' => new \Twig_Function_Method($this, 'getQrCodeUrl'),
-            'gchart_pie_chart'      => new \Twig_Function_Method($this, 'gchartPieChart', array('is_safe' => array('html'))),
-            'gchart_candlestick_chart' => new \Twig_Function_Method($this, 'gchartCandleStickChart', array('is_safe' => array('html'))),
-            'gchart_column_chart'   => new \Twig_Function_Method($this, 'gchartColumnChart', array('is_safe' => array('html'))),
-            'gchart_line_chart'     => new \Twig_Function_Method($this, 'gchartLineChart', array('is_safe' => array('html'))),
-            'gchart_bar_chart'      => new \Twig_Function_Method($this, 'gchartBarChart', array('is_safe' => array('html'))),
-            'gchart_area_chart'     => new \Twig_Function_Method($this, 'gchartAreaChart', array('is_safe' => array('html'))),
-            'gchart_treemap'     => new \Twig_Function_Method($this, 'gchartTreeMap', array('is_safe' => array('html'))),
-            'gchart_scatter_chart'  => new \Twig_Function_Method($this, 'gchartScatterChart', array('is_safe' => array('html'))),
-            'gchart_combo_chart'    => new \Twig_Function_Method($this, 'gchartComboChart', array('is_safe' => array('html'))),
-            'gchart_gauge'          => new \Twig_Function_Method($this, 'gchartGauge', array('is_safe' => array('html'))),
-            'gchart_table'          => new \Twig_Function_Method($this, 'gchartTable', array('is_safe' => array('html'))),
-            'gchart_get_pie_chart_url' => new \Twig_Function_Method($this, 'getPieChartUrl', array('is_safe' => array('html'))),
+            'gchart_area_chart'          => new \Twig_Function_Method($this, 'gchartAreaChart', array('is_safe' => array('html'))),
+            'gchart_bar_chart'           => new \Twig_Function_Method($this, 'gchartBarChart', array('is_safe' => array('html'))),
+            'gchart_calendar'           => new \Twig_Function_Method($this, 'gchartCalendar', array('is_safe' => array('html'))),
+            'gchart_candlestick_chart'   => new \Twig_Function_Method($this, 'gchartCandleStickChart', array('is_safe' => array('html'))),
+            'gchart_column_chart'        => new \Twig_Function_Method($this, 'gchartColumnChart', array('is_safe' => array('html'))),
+            'gchart_combo_chart'         => new \Twig_Function_Method($this, 'gchartComboChart', array('is_safe' => array('html'))),
+            'gchart_gauge'               => new \Twig_Function_Method($this, 'gchartGauge', array('is_safe' => array('html'))),
+            'gchart_get_icon_pin_url'    => new \Twig_Function_Method($this, 'getIconPinUrl', array('is_safe' => array('html'))),
+            'gchart_get_icon_url'        => new \Twig_Function_Method($this, 'getIconUrl', array('is_safe' => array('html'))),
+            'gchart_get_letter_pin_url'  => new \Twig_Function_Method($this, 'getLetterPinUrl', array('is_safe' => array('html'))),
+            'gchart_get_pie_chart_url'   => new \Twig_Function_Method($this, 'getPieChartUrl', array('is_safe' => array('html'))),
             'gchart_get_pie_chart3d_url' => new \Twig_Function_Method($this, 'getPieChart3DUrl', array('is_safe' => array('html'))),
-            'gchart_get_icon_url'   => new \Twig_Function_Method($this, 'getIconUrl', array('is_safe' => array('html'))),
-            'gchart_get_letter_pin_url'   => new \Twig_Function_Method($this, 'getLetterPinUrl', array('is_safe' => array('html'))),
-            'gchart_get_icon_pin_url'   => new \Twig_Function_Method($this, 'getIconPinUrl', array('is_safe' => array('html'))),
+            'gchart_get_qrcode_url'      => new \Twig_Function_Method($this, 'getQrCodeUrl'),
+            'gchart_line_chart'          => new \Twig_Function_Method($this, 'gchartLineChart', array('is_safe' => array('html'))),
+            'gchart_pie_chart'           => new \Twig_Function_Method($this, 'gchartPieChart', array('is_safe' => array('html'))),
+            'gchart_scatter_chart'       => new \Twig_Function_Method($this, 'gchartScatterChart', array('is_safe' => array('html'))),
+            'gchart_table'               => new \Twig_Function_Method($this, 'gchartTable', array('is_safe' => array('html'))),
+            'gchart_treemap'             => new \Twig_Function_Method($this, 'gchartTreeMap', array('is_safe' => array('html'))),
             
         );
     }
     
     /**
-     * gchart_pie_chart definition
+     * gchart_area_chart definition
      */
-    public function gchartPieChart($data, $id, $width, $height, $title = null, $config = array()) {
-        return $this->renderGChart($data, $id, 'PieChart', $width, $height, $title, $config);
+    public function gchartAreaChart($data, $id, $width, $height, $title = null, $config = array(), $events = array()) {
+        return $this->renderGChart($data, $id, 'AreaChart', $width, $height, $title, $config, false, $events);
     }
+    
     /**
-     * gchart_column_chart definition
+     * gchart_bar_chart definition
      */
-    public function gchartColumnChart($data, $id, $width, $height, $title = null, $config = array()) {
-        return $this->renderGChart($data, $id, 'ColumnChart', $width, $height, $title, $config);
+    public function gchartBarChart($data, $id, $width, $height, $title = null, $config = array(), $events = array()) {
+        return $this->renderGChart($data, $id, 'BarChart', $width, $height, $title, $config, false, $events);
+    }
+    
+    /**
+     * gchart_calendar definition
+     */
+    public function gchartCalendar($data, $id, $width, $height, $title = null, $config = array(), $events = array()) {
+        return $this->renderGChart($data, $id, 'Calendar', $width, $height, $title, $config, false, $events);
     }
     
     /**
      * gchart_candlestick_chart definition - needs 5 cols
      * @see http://code.google.com/apis/chart/interactive/docs/gallery/candlestickchart.html#Data_Format
      */
-    public function gchartCandleStickChart($data, $id, $width, $height, $title = null, $config = array()) {
-        return $this->renderGChart($data, $id, 'CandlestickChart', $width, $height, $title, $config);
+    public function gchartCandleStickChart($data, $id, $width, $height, $title = null, $config = array(), $events = array()) {
+        return $this->renderGChart($data, $id, 'CandlestickChart', $width, $height, $title, $config, false, $events);
     }
+    
+    /**
+     * gchart_column_chart definition
+     */
+    public function gchartColumnChart($data, $id, $width, $height, $title = null, $config = array(), $events = array()) {
+        return $this->renderGChart($data, $id, 'ColumnChart', $width, $height, $title, $config, false, $events);
+    }
+    
+    /**
+     * gchart_combo_chart definition
+     */
+    public function gchartComboChart($data, $id, $width, $height, $seriesType = 'line', $title = null, $config = array(), $events = array()) {
+        if (isset($seriesType) && !isset($config['seriesType'])) {
+            $config['seriesType'] = $seriesType;
+        }
+        return $this->renderGChart($data, $id, 'ComboChart', $width, $height, $title, $config, false, $events);
+    }
+    
+    /**
+     * gchart_gauge definition
+     */
+    public function gchartGauge($data, $id, $width, $height, $title = null, $config = array(), $events = array()) {
+        return $this->renderGChart($data, $id, 'Gauge', $width, $height, $title, $config, false, $events);
+    }
+    
     /**
      * gchart_line_chart definition
      */
-    public function gchartLineChart($data, $id, $width, $height, $title = null, $config = array()) {
-        return $this->renderGChart($data, $id, 'LineChart', $width, $height, $title, $config);
-    }
-    /**
-     * gchart_bar_chart definition
-     */
-    public function gchartBarChart($data, $id, $width, $height, $title = null, $config = array()) {
-        return $this->renderGChart($data, $id, 'BarChart', $width, $height, $title, $config);
+    public function gchartLineChart($data, $id, $width, $height, $title = null, $config = array(), $events = array()) {
+        return $this->renderGChart($data, $id, 'LineChart', $width, $height, $title, $config, false, $events);
     }
     
     /**
-     * gchart_area_chart definition
+     * gchart_pie_chart definition
      */
-    public function gchartAreaChart($data, $id, $width, $height, $title = null, $config = array()) {
-        return $this->renderGChart($data, $id, 'AreaChart', $width, $height, $title, $config);
-    }
-    
-    /**
-     * gchart_treemap definition - needs 4 cols
-     * @see http://code.google.com/apis/chart/interactive/docs/gallery/treemap.html#Data_Format
-     */
-    public function gchartTreeMap($data, $id, $width, $height, $title = '', $config = array()) {
-        return $this->renderGChart($data, $id, 'TreeMap', $width, $height, $title, $config, true);
+    public function gchartPieChart($data, $id, $width, $height, $title = null, $config = array(), $events = array()) {
+        return $this->renderGChart($data, $id, 'PieChart', $width, $height, $title, $config, false, $events);
     }
     
     /**
      * gchart_column_chart definition
      * note: The x-axis column cannot be of type string
      */
-    public function gchartScatterChart($data, $id, $width, $height, $title = null, $xLabel = null, $yLabel = null, $config = array()) {
+    public function gchartScatterChart($data, $id, $width, $height, $title = null, $xLabel = null, $yLabel = null, $config = array(), $events = array()) {
         if (!is_null($xLabel)) {
             $hAxis = isset($config['hAxis'])? $config['hAxis']: array();
             $hAxis['title'] = $xLabel;
@@ -112,42 +132,32 @@ class GChartExtension extends \Twig_Extension {
             $config['vAxis'] = $vAxis;
         }
         
-        return $this->renderGChart($data, $id, 'ScatterChart', $width, $height, $title, $config);
-    }
-    
-    /**
-     * gchart_combo_chart definition
-     */
-    public function gchartComboChart($data, $id, $width, $height, $seriesType = 'line', $title = null, $config = array()) {
-        if (isset($seriesType) && !isset($config['seriesType'])) {
-            $config['seriesType'] = $seriesType;
-        }
-        return $this->renderGChart($data, $id, 'ComboChart', $width, $height, $title, $config);
-    }
-    
-    /**
-     * gchart_gauge definition
-     */
-    public function gchartGauge($data, $id, $width, $height, $title = null, $config = array()) {
-        return $this->renderGChart($data, $id, 'Gauge', $width, $height, $title, $config);
+        return $this->renderGChart($data, $id, 'ScatterChart', $width, $height, $title, $config, false, $events);
     }
     
     /**
      * gchart_table definition
      */
-    public function gchartTable($data, $id, $config = null) {
-        return $this->renderTemplate('gChartTemplate', array('chartType' => 'Table', 'data' => $data, 'id' => $id, 'config' => $config ));
+    public function gchartTable($data, $id, $config = null, $events = array()) {
+        return $this->renderTemplate('gChartTemplate', array('chartType' => 'Table', 'data' => $data, 'id' => $id, 'config' => $config, 'events' => $events ));
     }
     
+    /**
+     * gchart_treemap definition - needs 4 cols
+     * @see http://code.google.com/apis/chart/interactive/docs/gallery/treemap.html#Data_Format
+     */
+    public function gchartTreeMap($data, $id, $width, $height, $title = '', $config = array(), $events = array()) {
+        return $this->renderGChart($data, $id, 'TreeMap', $width, $height, $title, $config, true, $events);
+    }
     
     /**
      * Generic method that returns html of gchart charts
      */
-    protected function renderGChart($data, $id, $type, $width, $height, $title = null, $config = array(), $addDivWithAndHeight = false) {
+    protected function renderGChart($data, $id, $type, $width, $height, $title = null, $config = array(), $addDivWithAndHeight = false, $events = array()) {
         $config['width'] = $width;
         $config['height'] = $height;
         if (!isset($config['title']) && !is_null($title) && trim($title) != '') { $config['title'] = $title;}
-        return $this->renderTemplate('gChartTemplate', array('chartType' => $type, 'data' => $data, 'id' => $id, 'config' => $config ), $addDivWithAndHeight);
+        return $this->renderTemplate('gChartTemplate', array('chartType' => $type, 'data' => $data, 'id' => $id, 'config' => $config, 'events' => $events  ), $addDivWithAndHeight);
     }
     
     /**
